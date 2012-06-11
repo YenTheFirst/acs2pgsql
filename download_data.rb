bba757e5701a8f32b7e62375592e93c5c2ba0370
T S <yenthefirst@gmail.com>
2012-06-11 14:21:34 -0700

    ensure that prepare_acs doesn't clobber CWD

--- download_data.rb
+++ download_data.rb
@@ -11,26 +11,35 @@ def prepare_acs_subject_file(seqnum)
     File.exists? filename
   end
 
-  #download all the data, for each state, and each geography type
-  `wget -r -nc -np -nH --cut-dirs=4 'ftp://ftp.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_By_State_By_Sequence_Table_Subset' -A '*#{seqnum}.zip'`
+  current_dir = Dir.pwd
 
-  #unzip our data, get list of filenames
-  Dir.chdir('All_Geographies_Not_Tracts_Block_Groups')
-    `unzip '*#{seqnum}.zip'`
-  Dir.chdir('..')
+  begin
 
-  Dir.chdir("Tracts_Block_Groups_Only")
-    `unzip '*#{seqnum}.zip'`
-    files = Dir.glob("[me]*txt")
-  Dir.chdir("..")
+    #download all the data, for each state, and each geography type
+    `wget -r -nc -np -nH --cut-dirs=4 'ftp://ftp.census.gov/acs2010_5yr/summaryfile/2006-2010_ACSSF_By_State_By_Sequence_Table_Subset' -A '*#{seqnum}.zip'`
 
-  #merge the downloaded data into a usable subject file
-  STATES.each do |state|
-    ['m','e'].each do |type|
-      filename = "#{type}20105#{state}#{seqnum}.txt"
-      `sort -k26,32 -m All_Geographies_Not_Tracts_Block_Groups/#{filename} Tracts_Block_Groups_Only/#{filename} -o all_data/#{filename}`
+    #unzip our data, get list of filenames
+    Dir.chdir('All_Geographies_Not_Tracts_Block_Groups')
+      `unzip '*#{seqnum}.zip'`
+    Dir.chdir('..')
+
+    Dir.chdir("Tracts_Block_Groups_Only")
+      `unzip '*#{seqnum}.zip'`
+      files = Dir.glob("[me]*txt")
+    Dir.chdir("..")
+
+    #merge the downloaded data into a usable subject file
+    STATES.each do |state|
+      ['m','e'].each do |type|
+        filename = "#{type}20105#{state}#{seqnum}.txt"
+        `sort -k26,32 -m All_Geographies_Not_Tracts_Block_Groups/#{filename} Tracts_Block_Groups_Only/#{filename} -o all_data/#{filename}`
+      end
     end
+
+  ensure
+    Dir.chdir(current_dir)
   end
+
 
 end
 
